0a8a63d09531acbf3f8f4e74e1a9c9bb0e0d606c
cornelia <corneliakelinske@gmail.com>
2023-07-09 07:52:31 -0700

    add check guess function and test

--- lib/braini_ex/word_games.ex
+++ lib/braini_ex/word_games.ex
@@ -8,4 +8,7 @@ defmodule BrainiEx.WordGames do
 
   @spec start_wordle_game() :: Game.t()
   defdelegate start_wordle_game, to: Wordle, as: :create_game
+
+  @spec check_guess(String.t(), Game.t()) :: Game.t()
+  defdelegate check_guess(guess, game), to: Wordle, as: :check_guess_and_update_game
 end
