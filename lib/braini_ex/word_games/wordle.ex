9613747474fd104122392fe363937d893750670b
cornelia <corneliakelinske@gmail.com>
2023-07-09 07:39:09 -0700

    refactor to avoid duplication

--- lib/braini_ex/word_games/wordle.ex
+++ lib/braini_ex/word_games/wordle.ex
@@ -18,45 +18,39 @@ defmodule BrainiEx.WordGames.Wordle do
       ) do
     guess = String.downcase(guess)
 
-    if guess === secret_word do
-      color_feedback =
-        guess
-        |> String.graphemes()
-        |> Enum.map(&{&1, :green})
-
-      Map.merge(game, %{
-        current_guess: guess,
-        won: true,
-        attempts: attempts + 1,
-        color_feedback: color_feedback
-      })
-    else
-      guess_letters = String.graphemes(guess)
-      secret_word_letters = String.graphemes(secret_word)
-      color_feedback = check_letters(guess_letters, secret_word_letters)
-
-      Map.merge(game, %{
-        current_guess: guess,
-        won: false,
-        attempts: attempts + 1,
-        color_feedback: color_feedback
-      })
-    end
+    update_params = game_updates(guess, secret_word, attempts)
+    Map.merge(game, update_params)
   end
 
   defp word do
     Enum.random(@words)
   end
 
+  defp game_updates(guess, secret_word, attempts) do
+    if guess === secret_word do
+      color_feedback =
+        guess
+        |> String.graphemes()
+        |> Enum.map(&{&1, :green})
+
+      %{color_feedback: color_feedback, won: true, current_guess: guess, attempts: attempts + 1}
+    else
+      guess_letters = String.graphemes(guess)
+      secret_word_letters = String.graphemes(secret_word)
+      color_feedback = check_letters(guess_letters, secret_word_letters)
+      %{color_feedback: color_feedback, current_guess: guess, attempts: attempts + 1}
+    end
+  end
+
   defp check_letters(guess_letters, secret_word_letters) do
     guess_letters
     |> Enum.zip(secret_word_letters)
-    |> Enum.map(&compare(&1, secret_word_letters))
+    |> Enum.map(&compare_guessed_letter(&1, secret_word_letters))
   end
 
-  defp compare({guessed_letter, guessed_letter}, _), do: {guessed_letter, :green}
+  defp compare_guessed_letter({guessed_letter, guessed_letter}, _), do: {guessed_letter, :green}
 
-  defp compare({guessed_letter, _}, secret_word_letters) do
+  defp compare_guessed_letter({guessed_letter, _}, secret_word_letters) do
     if guessed_letter in secret_word_letters do
       {guessed_letter, :yellow}
     else
